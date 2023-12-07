93d7b708c07aa78f23f904ce97e481f137f8b3b1
Orzech <hoch.mat@gmail.com>
2023-12-07 12:10:48 +0100

    Added ability to create a Visit. Closes #57

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/VisitControllerTest.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.Test;
+
+class VisitControllerTest {
+
+    @Test
+    void initCreateVisitForm() {
+    }
+
+    @Test
+    void processCreateVisitForm() {
+    }
+}
